c50d5c32cbbf4f648ca361094852d6891e3cdea2
wq17784411453 <944905972@qq.com>
2018-04-11 19:44:04 +0800

    关于数据库操作

---
+++ src/sqlbean/Connsql.java
@@ -0,0 +1,26 @@
+package sqlbean;
+import com.*;
+import servlet.*;
+import java.sql.*;
+
+public class Connsql {
+    public Connsql(){}
+    //连接数据库
+    String driver="org.gjt.mm.mysql.Driver";
+    String uesr="root";
+    String userPassword="wq123456789";
+    String url="jdbc:mysql://localhost:3306/badminton";
+    public Connection getConnection(){
+    	Connection con=null;
+		try
+		{
+			Class.forName(driver);
+			con=DriverManager.getConnection(url,uesr,userPassword);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		return con;
+    }
+}
